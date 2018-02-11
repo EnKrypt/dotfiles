a3c20737d29be9b06eefbff2bc120c3a5327393b
Arvind Kumar <arvind@enkrypt.in>
2018-02-12 02:17:18 +0530

    Modified dnscrypt-wrapper key rotation procedure

--- homedir/dnscrypt-wrapper/.dw/start.sh
+++ homedir/dnscrypt-wrapper/.dw/start.sh
@@ -2,4 +2,4 @@
 
 cd /home/arvind/.dw/
 
-nohup /usr/local/sbin/dnscrypt-wrapper --resolver-address=127.0.0.1:53 --listen-address=0.0.0.0:5353 --provider-name=2.dnscrypt-cert.arvind.io --crypt-secretkey-file=old.key,new.key --provider-cert-file=old.cert,new.cert > dnscrypt-wrapper.log 2>&1 &
+nohup /usr/local/sbin/dnscrypt-wrapper --resolver-address=127.0.0.1:53 --listen-address=0.0.0.0:5353 --provider-name=2.dnscrypt-cert.arvind.io --crypt-secretkey-file=old.key,new.key --provider-cert-file=old.cert,new.cert -V > dnscrypt-wrapper.log 2>&1 &
