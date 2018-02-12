628adc63bfb5a4c57f851b370cd4aa8c01269bbf
Arvind Kumar <arvind@enkrypt.in>
2018-02-12 19:54:53 +0530

    Removed dnscrypt-wrapper stuff

--- homedir/dnscrypt-wrapper/.dw/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/arvind/.dw/
-
-nohup /usr/local/sbin/dnscrypt-wrapper --resolver-address=127.0.0.1:53 --listen-address=0.0.0.0:5353 --provider-name=2.dnscrypt-cert.arvind.io --crypt-secretkey-file=old.key,new.key --provider-cert-file=old.cert,new.cert -V > dnscrypt-wrapper.log 2>&1 &
